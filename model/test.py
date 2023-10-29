bfeb00f3c409b9691df540e7189ff20f7f95a20d
arghyawning <arghya.roy@research.iiit.ac.in>
2023-10-29 13:20:57 +0530

    integrated our final finetuned model with frontend

--- model/test.py
+++ model/test.py
@@ -6,7 +6,12 @@ from langchain.llms import CTransformers
 from langchain.chains import RetrievalQA
 import chainlit as cl
 
-DB_FAISS_PATH = 'vectorstore/db_faiss'
+from flask import Flask, request, jsonify
+from transformers import pipeline
+from flask_cors import CORS
+
+
+DB_FAISS_PATH = "vectorstore/db_faiss"
 
 custom_prompt_template = """Use the following pieces of information to answer the user's question.
 If you don't know the answer, just say that you don't know, don't try to make up an answer.
@@ -18,39 +23,47 @@ Only return the helpful answer below and nothing else.
 Helpful answer:
 """
 
+
 def set_custom_prompt():
     """
     Prompt template for QA retrieval for each vectorstore
     """
-    prompt = PromptTemplate(template=custom_prompt_template,
-                            input_variables=['context', 'question'])
+    prompt = PromptTemplate(
+        template=custom_prompt_template, input_variables=["context", "question"]
+    )
     return prompt
 
-#Retrieval QA Chain
+
+# Retrieval QA Chain
 def retrieval_qa_chain(llm, prompt, db):
-    qa_chain = RetrievalQA.from_chain_type(llm=llm,
-                                       chain_type='stuff',
-                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
-                                       return_source_documents=True,
-                                       chain_type_kwargs={'prompt': prompt}
-                                       )
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=db.as_retriever(search_kwargs={"k": 2}),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt},
+    )
     return qa_chain
 
-#Loading the model
+
+# Loading the model
 def load_llm():
     # Load the locally downloaded model here
     llm = CTransformers(
-        model = "TheBloke/Llama-2-7B-Chat-GGML",
+        model="TheBloke/Llama-2-7B-Chat-GGML",
         model_type="llama",
-        max_new_tokens = 512,
-        temperature = 0.5
+        max_new_tokens=512,
+        temperature=0.5,
     )
     return llm
 
-#QA Model Function
+
+# QA Model Function
 def qa_bot():
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                       model_kwargs={'device': 'cpu'})
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"},
+    )
     db = FAISS.load_local(DB_FAISS_PATH, embeddings)
     llm = load_llm()
     qa_prompt = set_custom_prompt()
@@ -59,37 +72,36 @@ def qa_bot():
 
     return qa
 
-#output function
+
+# output function
 def final_result(query):
     qa_result = qa_bot()
-    response = qa_result({'query': query})
-    return response
+    response = qa_result({"query": query})
+    print("hidvhusvbuosdvhsodvhwovhivshdvisdvhsivhvipvsh")
+    print("--------------------------------------------------")
+    print(response["result"])
+    return response["result"]
 
-# #chainlit code
-@cl.on_chat_start
-async def start():
-    chain = qa_bot()
-    msg = cl.Message(content="Starting the bot...")
-    await msg.send()
-    msg.content = "Hi, Welcome to Medical Bot. What is your query?"
-    await msg.update()
 
-    cl.user_session.set("chain", chain)
+app = Flask(__name__)
+CORS(app)
 
-@cl.on_message
-async def main(message: cl.Message):
-    chain = cl.user_session.get("chain") 
-    cb = cl.AsyncLangchainCallbackHandler(
-        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
-    )
-    cb.answer_reached = True
-    res = await chain.acall(message.content, callbacks=[cb])
-    answer = res["result"]
-    sources = res["source_documents"]
 
-    if sources:
-        answer += f"\nSources:" + str(sources)
-    else:
-        answer += "\nNo sources found"
+@app.route("/get_answer", methods=["POST"])
+def get_answer():
+    data = request.get_json()
+    print(data)
+    question = data["question"]
+    # sample_question = input()
+    sample_question = question
+    # sample_question = "What are the symptoms of diabetes?"
 
-    await cl.Message(content=answer).send()
+    # Get an answer using the function
+    # answer = answer_medical_question(sample_question)
+    answer = final_result(sample_question)
+    print("Answer:", answer)
+    return jsonify({"answer": answer})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
