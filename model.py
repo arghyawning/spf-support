9e063ddeddd23d6e953b5d0b38c84833c769afdc
arghyawning <arghya.roy@research.iiit.ac.in>
2023-10-29 07:05:53 +0530

    tried other model. didnt work out well

--- model.py
+++ model.py
@@ -7,9 +7,16 @@ from flask_cors import CORS
 def answer_medical_question(question):
     # Load the GPT-2 model for text generation
     generator = pipeline("text-generation", model="gpt2")
+    # pl = pipeline(
+    #     "text-generation",
+    #     model="medalpaca/medalpaca-13b",
+    #     tokenizer="medalpaca/medalpaca-13b",
+    # )
 
     # Generate an answer based on the input question
     answer = generator(question, max_length=100, num_return_sequences=1)
+    # answer = pl(question, max_length=50, do_sample=True, temperature=0.9)
+    # print(answer[0]["generated_text"])
     return answer[0]["generated_text"]
 
 
